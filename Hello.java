43e06e8482f29e95ef1eb66f8f8baf875cc00c2f
hhghkh <muindindolo@proton.me>
2024-02-21 20:29:51 +0300

    added another

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello git");
         System.out.println("This is a new line added in IntelliJ");
+        System.out.println("and another one");
     }
 }
